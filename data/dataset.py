2b85b5ad4c6672aeb0c702e6c6b10497c6373af3
welu3614 <welu3614@uni.sydney.edu.au>
2024-11-25 02:12:25 -0500

    dataset adapt to zero-length input

--- data/dataset.py
+++ data/dataset.py
@@ -48,10 +48,12 @@ class WiderFaceDataset(Dataset):
         # raw coords, label: [[xmin, ymin, width, height], ...] (seq_len, 4)
         # coords [[y, x], [y, x]...]
         coords = np.zeros((label.shape[0], 2))
-        coords[:, 1] = label[:, 0] + (label[:, 2]-1)/2
-        coords[:, 0] = label[:, 1] + (label[:, 3]-1)/2
-        
-        img, coords = self.resize_and_transform_coord(img, coords, self.train)
+        if len(coords) > 0:
+            coords[:, 1] = label[:, 0] + (label[:, 2]-1)/2
+            coords[:, 0] = label[:, 1] + (label[:, 3]-1)/2
+            img, coords = self.resize_and_transform_coord(img, coords, self.train)
+        else:
+            img, coords = self.resize(img, self.train)
         # sample: (batch, 3, H, W)
         # coordinates: [batch, seq_len, 2]
         # heatmaps: ? [batch, seq_len, 1, H, W] (TYPICALLY GENERATED ON THE FLY)
@@ -86,6 +88,25 @@ class WiderFaceDataset(Dataset):
         coords[:,1] = np.clip(coords[:,1] * w_ratio, 0, 383) # x coord
         return img, coords
 
+    def resize(self, img, train):
+        if train:
+            transform = transforms.Compose([
+                    transforms.Resize((384, 384), interpolation=transforms.InterpolationMode.LANCZOS),
+                    TrackableRandomHorizontalFlip(p=0.5),
+                    transforms.ToTensor(),  # Convert to tensor
+                    transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+                ])
+            img = transform(img)
+        else:
+            transform = transforms.Compose([
+                    transforms.Resize((384, 384), interpolation=transforms.InterpolationMode.LANCZOS),
+                    transforms.ToTensor(),  # Convert to tensor
+                    transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+                ])
+            img = transform(img)
+        
+        return img, np.array([])
+
     def custom_collate_fn(self, batch):
         images = torch.stack([item[0] for item in batch])  # Images
         seq_lens = torch.tensor([len(item[1]) for item in batch], dtype=torch.long)
@@ -94,7 +115,8 @@ class WiderFaceDataset(Dataset):
         #coordinates = [item[1] for item in batch]  # List of variable-length tensors
         for i, item in enumerate(batch):
             seq_len = seq_lens[i]  # Current sequence length
-            coordinates[i, :seq_len, :] = torch.tensor(item[1], dtype=torch.float)  # Copy label into coordinates
+            if len(item[1]) > 0:
+                coordinates[i, :seq_len, :] = torch.tensor(item[1], dtype=torch.float)  # Copy label into coordinates
         return images, coordinates, seq_lens, max_seq_len
 
 
