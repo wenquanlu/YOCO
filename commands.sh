7aa1582752ec0329d6d4724a3080daeb34b180ad
Wenquan Lu <wenquanlu.hz@gmail.com>
2024-12-05 04:01:54 +0000

    clean up

--- commands.sh
+++
@@ -1,9 +0,0 @@
-python train.py --max_count=2 > log.txt
-
-python train.py --max_count=3 --state_dict=run_2/model_state_19_2.pth > log.txt
-
-python train.py --max_count=4 --state_dict=run_2/model_state_25_3.pth > log_1234.txt
-
-python train.py --max_count=5 --state_dict=run_2/model_state_9_4.pth > log_12345.txt
-
-
--- eval.sh
+++
@@ -1,4 +0,0 @@
-python eval.py --state_dict=run_1/model_state_34_5.pth --max_count=5
-
-
-
